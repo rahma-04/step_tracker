e44757588892a36b5e396f1dc43acfea91c3cf5f
Tiara <tiarafirdausa94@gmail.com>
2023-06-17 15:51:08 +0700

    layout

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables
 
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
@@ -17,21 +16,25 @@ class _StepTrackerPageState extends State<StepTrackerPage> {
   StreamSubscription<GyroscopeEvent>? _gyroscopeSubscription;
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
+  Stopwatch stopwatch = Stopwatch();
+  Timer? timer;
 
   @override
   void initState() {
     super.initState();
     _gyroscopeSubscription = gyroscopeEvents.listen((GyroscopeEvent event) {
       if (event.x > 2.0 || event.y > 2.0 || event.z > 2.0) {
-        setState(() {
-          stepCount++;
-          totalDistance = (stepCount * 0.73) / 1000;
-          totalCalories = (stepCount *
-                  double.parse(weightController.text) *
-                  double.parse(heightController.text) *
-                  25) /
-              (1000 * 45 * 162.7);
-        });
+        if (stopwatch.isRunning) {
+          setState(() {
+            stepCount++;
+            totalDistance = (stepCount * 0.73) / 1000;
+            totalCalories = (stepCount *
+                    double.parse(weightController.text) *
+                    double.parse(heightController.text) *
+                    25) /
+                (1000 * 45 * 162.7);
+          });
+        }
       }
     });
   }
@@ -42,6 +45,8 @@ class _StepTrackerPageState extends State<StepTrackerPage> {
     _gyroscopeSubscription?.cancel();
     weightController.dispose();
     heightController.dispose();
+    stopwatch.stop();
+    timer?.cancel();
   }
 
   void resetStepCount() {
@@ -49,6 +54,23 @@ class _StepTrackerPageState extends State<StepTrackerPage> {
       stepCount = 0;
       totalDistance = 0.0;
       totalCalories = 0.0;
+      stopwatch.reset();
+    });
+  }
+
+  void startTimer() {
+    setState(() {
+      stopwatch.start();
+      timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
+        setState(() {});
+      });
+    });
+  }
+
+  void stopTimer() {
+    setState(() {
+      stopwatch.stop();
+      timer?.cancel();
     });
   }
 
@@ -57,75 +79,261 @@ class _StepTrackerPageState extends State<StepTrackerPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Step Tracker'),
+        backgroundColor: Color(0xff00b2ca),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Weight (kg):',
-              style: TextStyle(fontSize: 18),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    width: 250,
+                    height: 250,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          top: 16.0), // Tambahkan padding top di sini
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color(0xff00b2ca),
+                              Color.fromARGB(255, 211, 248, 255),
+                            ],
+                          ),
+                        ),
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                '$stepCount',
+                                style: TextStyle(
+                                  fontSize: 48,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topCenter,
+                              child: Icon(
+                                Icons.directions_walk,
+                                size: 48,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Weight (kg):',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            SizedBox(height: 8),
+                            TextFormField(
+                              keyboardType: TextInputType.number,
+                              controller: weightController,
+                              onChanged: (value) {
+                                setState(() {
+                                  // Nothing to do here as the value is updated through the controller
+                                });
+                              },
+                              style: TextStyle(fontSize: 16),
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Color.fromARGB(255, 224, 242, 246),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Height (cm):',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            SizedBox(height: 8),
+                            TextFormField(
+                              keyboardType: TextInputType.number,
+                              controller: heightController,
+                              onChanged: (value) {
+                                setState(() {
+                                  // Nothing to do here as the value is updated through the controller
+                                });
+                              },
+                              style: TextStyle(fontSize: 16),
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Color.fromARGB(255, 224, 242, 246),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    color: Color(0xfffbd1a2),
+                    child: Text(
+                      '${stopwatch.elapsed.inHours.toString().padLeft(2, '0')}:${(stopwatch.elapsed.inMinutes % 60).toString().padLeft(2, '0')}:${(stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, '0')}',
+                      style: TextStyle(fontSize: 24),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 180,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color(0xFF7dcfb6), // #f79256
+                              Colors.white,
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Icon(Icons.outdoor_grill),
+                            SizedBox(height: 8),
+                            Text(
+                              'Total Distance',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              '${totalDistance.toStringAsFixed(2)} Km',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 180,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color(0xFFF79256), // #f79256
+                              Colors.white,
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Icon(Icons.local_fire_department),
+                            SizedBox(height: 8),
+                            Text(
+                              'Total Calories',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              '${totalCalories.toStringAsFixed(2)} Kcal',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: startTimer,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xfffcbf49),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 16.0),
+                        ),
+                        child: Text(
+                          'Start',
+                          style: TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: stopTimer,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 114, 215, 232),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 16.0),
+                        ),
+                        child: Text(
+                          'Stop',
+                          style: TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: resetStepCount,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xffd62828),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 16.0),
+                        ),
+                        child: Text(
+                          'Reset Step',
+                          style: TextStyle(fontSize: 15.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 8),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              controller: weightController,
-              onChanged: (value) {
-                setState(() {
-                  // Nothing to do here as the value is updated through the controller
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Height (cm):',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 8),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              controller: heightController,
-              onChanged: (value) {
-                setState(() {
-                  // Nothing to do here as the value is updated through the controller
-                });
-              },
-            ),
-            SizedBox(height: 32),
-            Text(
-              'Step Count',
-              style: TextStyle(fontSize: 24),
-            ),
-            Text(
-              '$stepCount',
-              style: TextStyle(fontSize: 48),
-            ),
-            SizedBox(height: 24),
-            Text(
-              'Total Distance',
-              style: TextStyle(fontSize: 24),
-            ),
-            Text(
-              '${totalDistance.toStringAsFixed(2)} Km',
-              style: TextStyle(fontSize: 48),
-            ),
-            SizedBox(height: 24),
-            Text(
-              'Total Calories',
-              style: TextStyle(fontSize: 24),
-            ),
-            Text(
-              '${totalCalories.toStringAsFixed(2)} Kcal',
-              style: TextStyle(fontSize: 48),
-            ),
-            SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: resetStepCount,
-              child: Text('Reset Step'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
